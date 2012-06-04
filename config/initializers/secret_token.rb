5876a560c6d58e605bbfb64109e68483f3941821
Evan Kuchar <evan.kuchar@gmail.com>
2012-06-04 12:38:44 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedBucket::Application.config.secret_token = 'a4a7e4f1e64224dbf5a5bee75193a1174d2aa8e724c73824c4a9218a38d8509146371e4ceb927f020375b51487bac418052474fc5c24a0b2e07603f5fa6d9a80'
